e27f1153e37a89c7bdfe28b20e1d55290b1e1236
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2022-06-05 18:11:43 +0100

    Create 1699_calls_between_two_persons.sql

---
+++ src/1699_calls_between_two_persons.sql
@@ -0,0 +1,18 @@
+WITH cte AS (
+    SELECT 
+      *,
+      to_id > from_id AS flag
+    FROM 
+      Calls
+)
+
+SELECT 
+  IF(flag, from_id, to_id) person1,
+  IF(flag, to_id, from_id) person2,
+  COUNT(*) call_count,
+  SUM(duration) total_duration
+FROM 
+  cte
+GROUP BY 
+  1, 
+  2
