50c996c7ce378dc0699b8334193bc6af75f28e06
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2022-06-16 21:49:18 +0100

    Create 1212_team_scores_football.sql

---
+++ src/1212_team_scores_football.sql
@@ -0,0 +1,107 @@
+-- https://leetcode.com/problems/team-scores-in-football-tournament/
+--
+-- team_id, team_name and num_points of each team in the tournament
+WITH host AS (
+    SELECT
+        host_team team_id,
+        CASE 
+            WHEN host_goals > guest_goals 
+            THEN 3
+            WHEN host_goals = guest_goals
+            THEN 1
+            ELSE 0
+        END num_points
+    FROM
+        matches
+),
+guest AS (
+    SELECT
+        guest_team team_id,
+        CASE 
+            WHEN host_goals < guest_goals 
+            THEN 3
+            WHEN host_goals = guest_goals
+            THEN 1
+            ELSE 0
+        END num_points
+    FROM
+        matches
+),
+points AS (
+    SELECT 
+        * 
+    FROM
+        host
+    UNION ALL
+    SELECT 
+        *
+    FROM
+        guest
+),
+totals AS (
+    SELECT 
+        team_id,
+        SUM(num_points) num_points
+    FROM
+        points
+    GROUP BY
+        1
+
+)
+SELECT 
+    t.*,
+    COALESCE(s.num_points, 0) num_points
+FROM
+    Teams t
+LEFT JOIN
+    totals s
+USING
+    (team_id)
+ORDER BY
+    3 DESC,
+    1
+
+-- Alternative
+WITH diffs AS (
+    SELECT 
+        host_team,
+        guest_team,
+        (host_goals - guest_goals) goal_diff
+    FROM
+        Matches
+),
+comb AS (
+    SELECT 
+        host_team team_id, 
+        goal_diff 
+    FROM 
+        diffs
+    UNION ALL
+    SELECT 
+        guest_team team_id, 
+        -goal_diff goal_diff 
+    FROM 
+        diffs
+)
+
+SELECT
+    t1.*,
+    COALESCE(
+        SUM(CASE 
+                 WHEN t2.goal_diff > 0 THEN 3
+                 WHEN t2.goal_diff = 0 THEN 1
+                 ELSE 0
+             END)
+    ) num_points
+FROM
+    Teams t1
+LEFT JOIN
+    comb t2
+USING
+    (team_id)
+GROUP BY
+    1,
+    2
+ORDER BY
+    3 DESC,
+    1
