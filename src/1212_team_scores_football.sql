68221123ec346d4273e15195232955199002af65
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2022-06-16 21:50:06 +0100

    Update 1212_team_scores_football.sql

--- src/1212_team_scores_football.sql
+++ src/1212_team_scores_football.sql
@@ -1,6 +1,51 @@
 -- https://leetcode.com/problems/team-scores-in-football-tournament/
---
--- team_id, team_name and num_points of each team in the tournament
+
+-- Alternative
+WITH diffs AS (
+    SELECT 
+        host_team,
+        guest_team,
+        (host_goals - guest_goals) goal_diff
+    FROM
+        Matches
+),
+comb AS (
+    SELECT 
+        host_team team_id, 
+        goal_diff 
+    FROM 
+        diffs
+    UNION ALL
+    SELECT 
+        guest_team team_id, 
+        -goal_diff goal_diff 
+    FROM 
+        diffs
+)
+
+SELECT
+    t1.*,
+    COALESCE(
+        SUM(CASE 
+                 WHEN t2.goal_diff > 0 THEN 3
+                 WHEN t2.goal_diff = 0 THEN 1
+                 ELSE 0
+             END)
+    ) num_points
+FROM
+    Teams t1
+LEFT JOIN
+    comb t2
+USING
+    (team_id)
+GROUP BY
+    1,
+    2
+ORDER BY
+    3 DESC,
+    1
+    
+-- Naive
 WITH host AS (
     SELECT
         host_team team_id,
@@ -61,47 +106,4 @@ ORDER BY
     3 DESC,
     1
 
--- Alternative
-WITH diffs AS (
-    SELECT 
-        host_team,
-        guest_team,
-        (host_goals - guest_goals) goal_diff
-    FROM
-        Matches
-),
-comb AS (
-    SELECT 
-        host_team team_id, 
-        goal_diff 
-    FROM 
-        diffs
-    UNION ALL
-    SELECT 
-        guest_team team_id, 
-        -goal_diff goal_diff 
-    FROM 
-        diffs
-)
 
-SELECT
-    t1.*,
-    COALESCE(
-        SUM(CASE 
-                 WHEN t2.goal_diff > 0 THEN 3
-                 WHEN t2.goal_diff = 0 THEN 1
-                 ELSE 0
-             END)
-    ) num_points
-FROM
-    Teams t1
-LEFT JOIN
-    comb t2
-USING
-    (team_id)
-GROUP BY
-    1,
-    2
-ORDER BY
-    3 DESC,
-    1
