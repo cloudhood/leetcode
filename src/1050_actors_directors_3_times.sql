bbb76be51a3b91da33addeac5e9854a6927e9344
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2022-06-24 22:06:42 +0100

    Create 1050_actors_directors_3_times.sql

---
+++ src/1050_actors_directors_3_times.sql
@@ -0,0 +1,11 @@
+-- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+SELECT 
+    actor_id,
+    director_id
+FROM
+    ActorDirector
+GROUP BY
+    1,
+    2
+HAVING
+    COUNT(*) >= 3
