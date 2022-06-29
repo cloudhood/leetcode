fe74eed810ace5e6b13cb1422cf7df9e530c4e19
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2022-06-29 21:56:13 +0100

    Update 262_trips_and_users.sql

--- src/262_trips_and_users.sql
+++ src/262_trips_and_users.sql
@@ -3,28 +3,28 @@
 -- Need two joins, can't just do one join with OR.
 WITH unbanned AS (
     SELECT 
-		users_id
-    from 
-		Users
+	    users_id
+    FROM 
+	    Users
     WHERE 
-		banned = "No"
+	    banned = "No"
     AND 
-		role <> "partner"
+	    role <> "partner"
 )
 SELECT 
     request_at "Day",
     ROUND(SUM(IF(Status LIKE "cancelled%", 1, 0)) / COUNT(*), 2) "Cancellation Rate"   
 FROM 
-	Trips t
+    Trips t
 INNER JOIN
-	unbanned u1
+    unbanned u1
 ON 
-	t.client_id = u1.users_id
+    t.client_id = u1.users_id
 INNER JOIN 
-	unbanned u2
+    unbanned u2
 ON 
-	t.driver_id = u2.users_id
+    t.driver_id = u2.users_id
 WHERE 
-	request_at BETWEEN "2013-10-01" AND "2013-10-03"
+    request_at BETWEEN "2013-10-01" AND "2013-10-03"
 GROUP BY 
-	1
+    1
