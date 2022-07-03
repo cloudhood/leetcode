758723c9a7191cab5054e9760c73c81b9397e1c6
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2022-07-03 22:07:11 +0100

    Update 1225_report_contiguous_dates.sql

--- src/1225_report_contiguous_dates.sql
+++ src/1225_report_contiguous_dates.sql
@@ -1,11 +1,38 @@
--- https://leetcode.com/problems/report-contiguous-dates/ 
+-- https://leetcode.com/problems/report-contiguous-dates/
 --
--- WIP
+-- The key insight is to identify consecutive dates per state using a derived ROW_NUMBER variable.
+-- When difference between n1 and n2 changes, we have a new run.
+--
+-- period_date	period_state	n1		
+-- 28/12/2018	fail	        1		
+-- 29/12/2018	fail	        2		
+-- 04/01/2019	fail	        3   		
+-- 05/01/2019	fail	        4		
+				
+-- period_date	period_state	n1		
+-- 30/12/2018	success 	    1		
+-- 31/12/2018	success 	    2		
+-- 01/01/2019	success	        3		
+-- 02/01/2019	success 	    4		
+-- 03/01/2019	success	        5		
+-- 06/03/2019	success	        6		
+				
+-- period_date	period_state	n1	n2	n2 - n1
+-- 28/12/2018	fail	        1	1	0
+-- 29/12/2018	fail	        2	2	0
+-- 04/01/2019	fail	        3	8	5
+-- 05/01/2019	fail	        4	9	5
+-- 30/12/2018	success     	1	3	2
+-- 31/12/2018	success     	2	4	2
+-- 01/01/2019	success     	3	5	2
+-- 02/01/2019	success	        4	6	2
+-- 03/01/2019	success     	5	7	2
+-- 06/03/2019	success	        6	10	4
 WITH success AS (
     SELECT
         success_date period_date,
         "succeeded" period_state,
-        RANK() OVER (ORDER BY success_date) n1
+        ROW_NUMBER() OVER (ORDER BY success_date) n1 - ROW_NUMBER() works too
     FROM
         Succeeded
 ),
@@ -13,7 +40,7 @@ fails AS (
     SELECT
         fail_date period_date,
         "failed" period_state,
-        RANK() OVER (ORDER BY fail_date) n1
+        ROW_NUMBER() OVER (ORDER BY fail_date) n1
     FROM
         Failed
 ),
@@ -31,7 +58,7 @@ comb AS (
 comb2 AS (
     SELECT 
         *,
-        RANK() OVER (ORDER BY period_date) n2
+        ROW_NUMBER() OVER (ORDER BY period_date) n2
     FROM
         comb
     WHERE 
@@ -40,7 +67,7 @@ comb2 AS (
 comb3 AS (
     SELECT
         *,
-        (n2 - n1) n3
+        CAST(n2 AS SIGNED) - CAST(n1 AS SIGNED) diff
     FROM
         comb2
 )
@@ -49,10 +76,8 @@ SELECT
   period_state,
   MIN(period_date) start_date,
   MAX(period_date) end_date
-SELECT 
-    *
 FROM
     comb3
 GROUP BY
-    some_rank,
-    peroid_state
+    diff,
+    period_state
